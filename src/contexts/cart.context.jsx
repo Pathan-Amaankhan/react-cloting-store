4ccecc9ecffa2821d02abdafdfd703a39570544f
Amaan <amaanpathan5@gmail.com>
2022-12-06 15:58:48 +0530

    refactor: removes useState & adds useReducer for contexts

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,4 +1,4 @@
-import {createContext, useEffect, useState} from "react";
+import {createContext, useEffect, useReducer} from "react";
 
 const addCartItem = ( cartItems, productToAdd ) => {
 
@@ -29,49 +29,96 @@ const clearCartItem = ( cartItems, product ) => {
     return cartItems.filter( item => item.id !== product.id );
 }
 
-export const CartContext = createContext( {
+const INITIAL_STATE = {
     showCart: false,
-    setShowCart: () => null,
     cartItems: [],
-    addItemToCart: () => null,
-    removeItemFromCart: () => null,
-    clearItemFromCart: () => null,
     cartCount: 0,
     cartTotal: 0,
-} );
+}
 
+export const CartContext = createContext( INITIAL_STATE );
+
+const CART_ACTION_TYPE = {
+    UPDATE_SHOW_CART: 'UPDATE_SHOW_CART',
+    UPDATE_CART_ITEMS: 'UPDATE_CART_ITEMS',
+    UPDATE_CART_COUNT: 'UPDATE_CART_COUNT',
+    UPDATE_CART_TOTAL: 'UPDATE_CART_TOTAL',
+};
+
+const CartReducer = ( state, action ) => {
+    const { type, payload } = action;
+
+    switch ( type ) {
+        case CART_ACTION_TYPE.UPDATE_SHOW_CART:
+            return {
+                ...state,
+                showCart: payload.showCart,
+            }
+        case CART_ACTION_TYPE.UPDATE_CART_ITEMS:
+            const getCartItems = ( state, payload ) => {
+                switch ( payload.action ) {
+                    case 'add':
+                        return addCartItem( state.cartItems, payload.product );
+                    case 'remove':
+                        return removeCartItem( state.cartItems, payload.product );
+                    case 'clear':
+                        return clearCartItem( state.cartItems, payload.product );
+                    default:
+                        return state.cartItems;
+                }
+            };
+
+            return {
+                ...state,
+                cartItems: getCartItems( state, payload ),
+            }
+        case CART_ACTION_TYPE.UPDATE_CART_COUNT:
+            return {
+                ...state,
+                cartCount: payload.count
+            }
+        case CART_ACTION_TYPE.UPDATE_CART_TOTAL:
+            return {
+                ...state,
+                cartTotal: payload.total
+            }
+        default:
+            throw new Error( `Unhandled type ${type} in CartReducer` );
+    }
+}
 
 export const CartProvider = ( { children } ) => {
-    const [ showCart, setShowCart ] = useState( false );
-    const [ cartItems, setCartItems ] = useState( [] );
-    const [ cartCount, setCartCount ] = useState( 0 );
-    const [ cartTotal, setCartTotal ] = useState( 0 );
+    const [ { showCart, cartItems, cartCount, cartTotal }, dispatch ] = useReducer( CartReducer, INITIAL_STATE );
 
     useEffect( () => {
         const count = cartItems.reduce( ( accumulator, currentValue ) => accumulator + currentValue.quantity, 0 );
-        setCartCount( count );
+        dispatch( { type: CART_ACTION_TYPE.UPDATE_CART_COUNT,  payload: { count } } );
     }, [ cartItems ] );
 
     useEffect( () => {
         const total = cartItems.reduce( ( accumulator, currentValue ) => ( currentValue.quantity * currentValue.price ) + accumulator, 0 );
-        setCartTotal( total );
+        dispatch( { type: CART_ACTION_TYPE.UPDATE_CART_TOTAL, payload: { total } } );
     }, [ cartItems ] );
 
-    const addItemToCart = ( productToAdd ) => {
-        setCartItems( addCartItem( cartItems, productToAdd ) );
+    const addItemToCart = ( product ) => {
+        dispatch( { type: CART_ACTION_TYPE.UPDATE_CART_ITEMS, payload: { product, action: 'add' } } );
     }
 
     const removeItemFromCart = ( product ) => {
-        setCartItems( removeCartItem( cartItems, product ) );
+        dispatch( { type: CART_ACTION_TYPE.UPDATE_CART_ITEMS, payload: { product, action: 'remove' } } );
     }
 
     const clearItemFromCart = ( product ) => {
-        setCartItems( clearCartItem( cartItems, product )  );
+        dispatch( { type: CART_ACTION_TYPE.UPDATE_CART_ITEMS, payload: { product, action: 'clear' } } );
+    }
+
+    const setShowCart = ( showCart ) => {
+        dispatch( { type: CART_ACTION_TYPE.UPDATE_SHOW_CART, payload: { showCart } } );
     }
 
     const value = { showCart, setShowCart, cartItems, addItemToCart, cartCount, removeItemFromCart, cartTotal, clearItemFromCart };
     
-    return <CartContext.Provider value={value}>
+    return <CartContext.Provider value={ value }>
         { children }
     </CartContext.Provider>;
 }
